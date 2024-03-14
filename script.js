e8283d7cae19d20ace3e60f2b7ba5c3c6a2aff2d
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-03-14 15:32:45 -0700

    added for loop to playGame()

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 
 function playGame() {
     for (let i = 0; i < 5; i++) {
+        let playerSelection = window.prompt("Choose: Rock, Paper, or Scissors");
+        playerSelection = playerSelection.toLowerCase();
+        getComputerChoice();
         function playRound(playerSelection, computerSelection) {
         // your code here!
         }
