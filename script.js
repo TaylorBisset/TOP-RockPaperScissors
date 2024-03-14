26493a858c290434272b1ce56a179684a523c88d
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-03-14 15:43:19 -0700

    Add pseudocode to getComputerChoice()

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 // script.js
 
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    let computerSelection; 
+    
+    // if 1 then computerSelection = rock
+    // if 2 then computerSelection = paper
+    // if 3 then computerSelection = scissors
+
+    return computerSelection;
+}
+
 function playGame() {
     for (let i = 0; i < 5; i++) {
         let playerSelection = window.prompt("Choose: Rock, Paper, or Scissors");
